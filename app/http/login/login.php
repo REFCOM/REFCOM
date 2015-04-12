e33c269909fb227123f946bc4c2fb85692fa9e84
Brayan <brayan13sep@gmail.com>
2015-04-12 02:50:38 -0600

    Login
    Cambios en el css del login

--- app/http/login/login.php
+++ app/http/login/login.php
@@ -4,20 +4,29 @@
 echo '
 	<html>
 		<head>
+			<meta charset="utf-8">
+    		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+    		<!-- CSS de Bootstrap -->
+    		<link href="../../../bootstrap/dist/css/bootstrap.min.css" rel="stylesheet" media="screen">
+ 
 			<title>Login</title>
 		</head>
 		<body>
 			<h1> Iniciar Sesion </h1>
-			<form method= "POST">
-				<td> Usuario </td>
-				<br>
-				<input type="text" name="usuario" > 
-				<br>
-				<td>Contraseña </td>
-				<br>
-				<input type="text" name="password" >
-				<br>
-				<input type="submit" value="Iniciar Sesion">
+			<form class="form-inline" role="form" method= "POST">
+				<div class="form_group">
+					<td > Usuario </td>
+					<br>
+					<input type="text" name="usuario" class="form-control" placeholder="Usuario"> 
+					<br>
+					<td>Contraseña </td>
+					<br>
+					<input type="text" name="password" class="form-control" placeholder="Contraseña">
+					<br>
+					<input type="submit" value="Iniciar Sesion" class="btn btn-default">
+				</div>
 			</form>
 		</body>
 	</html>
