3fb7a505c7ca13d23c53e3ee59d3f7cc3d79cd9a
Brayan <brayan13sep@gmail.com>
2015-04-12 01:50:29 -0600

    BORRAR CODIGO BASURA
    SE BORRO EL CODIGO BASURA QUE GENERA MYSQL WORBENCH

--- database/script/REFCOM_v1.sql
+++ database/script/REFCOM_v1.sql
@@ -1,19 +1,3 @@
--- MySQL Workbench Forward Engineering
-
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
-
--- -----------------------------------------------------
--- Schema REFCOM
--- -----------------------------------------------------
-DROP SCHEMA IF EXISTS `REFCOM` ;
-
--- -----------------------------------------------------
--- Schema REFCOM
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `REFCOM` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
-USE `REFCOM` ;
 
 -- -----------------------------------------------------
 -- Table `REFCOM`.`CLIENTE`
@@ -228,7 +212,3 @@ CREATE TABLE IF NOT EXISTS `REFCOM`.`PRODUCTO_PROVEEDOR` (
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
